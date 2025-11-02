9c929a89c4c036386fbf199341ae77e663813491
Amaranth789 <gerardy.zhang@mail.utoronto.ca>
2025-11-02 11:30:58 -0500

    Update video path for validation match

--- user/validate.py
+++ user/validate.py
@@ -54,7 +54,7 @@ def test_agent_validation():
     logger.info("Validation match has started ...")
     run_match(my_agent,
             agent_2=opponent,
-            video_path=f'validate.mp4',
+            video_path=f'submission/results/validate.mp4',
             agent_1_name='Agent 1',
             agent_2_name='Agent 2',
             resolution=CameraResolution.LOW,
